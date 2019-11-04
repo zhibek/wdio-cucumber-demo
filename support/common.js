1d683513823aad18834fbcaf2d2ac72c81389288
Mohamed Mahdy <mohamed.mahdy@camelcasetech.com>
2019-11-04 17:01:00 +0200

    change isExist to waitForExist

--- support/common.js
+++ support/common.js
@@ -4,11 +4,8 @@
  * @param {string} selector to be checked
  * @returns {boolean} True if element exists or false if element doesn't exist
  */
-export function checkIfElementExists(selector, seconds = 3) {
-    return browser.waitUntil(() =>
-       $(selector).isExisting(),
-       (seconds * 1000)
-    );
+export function checkIfElementExists(selector) {
+    return $(selector).waitForExist();
 }
 
 /**
